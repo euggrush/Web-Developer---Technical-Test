9a1c8e722c756e359cb667a001f21148b776deab
evgenii.grushin <evgenii.grushin@fortify.pro>
2022-02-02 22:27:38 -0800

    work in progress

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
       <div class="row">
         <div class="col">
           <img
-            src="@/assets/img/profile.jpeg"
+            src="../assets/img/profile.jpeg"
             class="rounded-circle"
             alt="logo"
             width="120"
@@ -20,41 +20,41 @@
           />
         </div>
         <div class="col-6">
-          <h3>Nick Reynolds</h3>
+          <h3>{{ accountInfo.name }}</h3>
           <p>Bio</p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            {{ accountInfo.bio }}
           </p>
         </div>
         <div class="col">
           <p>Phone</p>
-          <a href="tel:+555-555-5555">555-555-5555</a>
+          <a href="tel:+555-555-5555">{{ accountInfo.phone }}</a>
 
           <p>Email</p>
-          <a href="mailto:nick.reynolds@domain.co">nick.reynolds@domain.co</a>
+          <a href="mailto:nick.reynolds@domain.co">{{ accountInfo.email }}</a>
         </div>
       </div>
     </div>
 
     <div class="pictures-wrap container mt-3">
       <div class="row row-cols-auto gap-3">
-        <div class="col image-wrap rounded p-0">
+        <div
+          v-for="(image, index) in accountInfo.album"
+          :key="index"
+          class="col image-wrap rounded p-0"
+        >
           <img
             src="@/assets/img/landscape1.jpeg"
             class="img-fluid"
             alt="image"
           />
           <div class="image-information">
-            <h4 class="text-white ps-1">Nandhaka Pieris</h4>
+            <h4 class="text-white ps-1">{{ image.title }}</h4>
             <span class="d-block pt-3 ps-3 pe-3 pb-1">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              {{ image.description }}
             </span>
             <div class="d-flex justify-content-between pt-1 ps-3 pe-3">
-              <div class="featured-icon">
+              <div v-show="image.featured" class="featured-icon">
                 <svg
                   width="24"
                   height="24"
@@ -68,158 +68,8 @@
                   />
                 </svg>
               </div>
-              <span>2015-05-01</span>
+              <span>{{ image.date }}</span>
             </div>
-          </div>
-        </div>
-        <div class="col image-wrap rounded p-0">
-          <img
-            src="@/assets/img/landscape1.jpeg"
-            class="img-fluid"
-            alt="image"
-          />
-
-          <h4>Nandhaka Pieris</h4>
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </span>
-          <div class="d-flex justify-content-between">
-            <div class="featured-icon">
-              <svg
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="m12.82 5.58-.82.822-.824-.824a5.375 5.375 0 1 0-7.601 7.602l7.895 7.895a.75.75 0 0 0 1.06 0l7.902-7.897a5.376 5.376 0 0 0-.001-7.599 5.38 5.38 0 0 0-7.611 0Z"
-                  fill="#FF0000"
-                />
-              </svg>
-            </div>
-            <span>2015-05-01</span>
-          </div>
-        </div>
-        <div class="col image-wrap rounded p-0">
-          <img
-            src="@/assets/img/landscape1.jpeg"
-            class="img-fluid"
-            alt="image"
-          />
-
-          <h4>Nandhaka Pieris</h4>
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </span>
-          <div class="d-flex justify-content-between">
-            <div class="featured-icon">
-              <svg
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="m12.82 5.58-.82.822-.824-.824a5.375 5.375 0 1 0-7.601 7.602l7.895 7.895a.75.75 0 0 0 1.06 0l7.902-7.897a5.376 5.376 0 0 0-.001-7.599 5.38 5.38 0 0 0-7.611 0Z"
-                  fill="#FF0000"
-                />
-              </svg>
-            </div>
-            <span>2015-05-01</span>
-          </div>
-        </div>
-        <div class="col image-wrap rounded p-0">
-          <img
-            src="@/assets/img/landscape1.jpeg"
-            class="img-fluid"
-            alt="image"
-          />
-
-          <h4>Nandhaka Pieris</h4>
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </span>
-          <div class="d-flex justify-content-between">
-            <div class="featured-icon">
-              <svg
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="m12.82 5.58-.82.822-.824-.824a5.375 5.375 0 1 0-7.601 7.602l7.895 7.895a.75.75 0 0 0 1.06 0l7.902-7.897a5.376 5.376 0 0 0-.001-7.599 5.38 5.38 0 0 0-7.611 0Z"
-                  fill="#FF0000"
-                />
-              </svg>
-            </div>
-            <span>2015-05-01</span>
-          </div>
-        </div>
-        <div class="col image-wrap rounded p-0">
-          <img
-            src="@/assets/img/landscape1.jpeg"
-            class="img-fluid"
-            alt="image"
-          />
-
-          <h4>Nandhaka Pieris</h4>
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </span>
-          <div class="d-flex justify-content-between">
-            <div class="featured-icon">
-              <svg
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="m12.82 5.58-.82.822-.824-.824a5.375 5.375 0 1 0-7.601 7.602l7.895 7.895a.75.75 0 0 0 1.06 0l7.902-7.897a5.376 5.376 0 0 0-.001-7.599 5.38 5.38 0 0 0-7.611 0Z"
-                  fill="#FF0000"
-                />
-              </svg>
-            </div>
-            <span>2015-05-01</span>
-          </div>
-        </div>
-        <div class="col image-wrap rounded">
-          <img
-            src="@/assets/img/landscape1.jpeg"
-            class="img-fluid"
-            alt="image"
-          />
-
-          <h4>Nandhaka Pieris</h4>
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </span>
-          <div class="d-flex justify-content-between">
-            <div class="featured-icon">
-              <svg
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="m12.82 5.58-.82.822-.824-.824a5.375 5.375 0 1 0-7.601 7.602l7.895 7.895a.75.75 0 0 0 1.06 0l7.902-7.897a5.376 5.376 0 0 0-.001-7.599 5.38 5.38 0 0 0-7.611 0Z"
-                  fill="#FF0000"
-                />
-              </svg>
-            </div>
-            <span>2015-05-01</span>
           </div>
         </div>
       </div>
@@ -228,15 +78,35 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
   },
+  data() {
+    return {
+      account: [],
+    };
+  },
+  computed: {
+    accountInfo() {
+      return this.account;
+    },
+  },
+  async mounted() {
+    await axios
+      .get(`http://localhost:3000/accounts`)
+      .then((response) => {
+        this.account = response.data[0];
+        console.log(this.account);
+      })
+      .catch((err) => console.log(err));
+  },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 p {
   margin: 0;
@@ -269,6 +139,7 @@ a {
 }
 .image-wrap {
   width: 100%;
+  min-height: 250px;
   background-color: $colorWhite;
   text-align: justify;
   @include media-breakpoint-up(md) {
